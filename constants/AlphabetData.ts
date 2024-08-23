52b46a9dbf16cc3c7866c359e6587e86e1a677e0
Chanel O'Reilly <chanel@shortfam.com>
2024-08-22 18:01:29 -0600

    Edited Main page and added images in assets and related file in constants

---
+++ constants/AlphabetData.ts
@@ -0,0 +1,142 @@
+export interface AlphabetType {
+    id: number;
+    title: string;
+    audio: string;
+}
+
+export const ALPHABET_DATA: AlphabetType[] = [
+    {
+        id: 1,
+        title: 'Aa',
+        audio: ' '
+    },
+    {
+        id: 2,
+        title: 'Bb',
+        audio: ' '
+    },
+    {
+        id: 3,
+        title: 'Cc',
+        audio: ' '
+    },
+    {
+        id: 4,
+        title: 'Dd',
+        audio: ' '
+    },
+    {
+        id: 5,
+        title: 'Ee',
+        audio: ' '
+    },
+    {
+        id: 6,
+        title: 'Ff',
+        audio: ' '
+    },
+    {
+        id: 7,
+        title: 'Gg',
+        audio: ' '
+    },
+    {
+        id: 8,
+        title: 'Hh',
+        audio: ' '
+    },
+    {
+        id: 9,
+        title: 'Ii',
+        audio: ' '
+    },
+    {
+        id: 10,
+        title: 'Jj',
+        audio: ' '
+    },
+    {
+        id: 11,
+        title: 'Kk',
+        audio: ' '
+    },
+    {
+        id: 12,
+        title: 'Ll',
+        audio: ' '
+    },
+    {
+        id: 13,
+        title: 'Mm',
+        audio: ' '
+    },
+    {
+        id: 14,
+        title: 'Nn',
+        audio: ' '
+    },
+    {
+        id: 15,
+        title: 'Oo',
+        audio: ' '
+    },
+    {
+        id: 16,
+        title: 'Pp',
+        audio: ' '
+    },
+    {
+        id: 17,
+        title: 'Qq',
+        audio: ' '
+    },
+    {
+        id: 18,
+        title: 'Rr',
+        audio: ' '
+    },
+    {
+        id: 19,
+        title: 'Ss',
+        audio: ' '
+    },
+    {
+        id: 20,
+        title: 'Tt',
+        audio: ' '
+    },
+    {
+        id: 21,
+        title: 'Uu',
+        audio: ' '
+    },
+    {
+        id: 22,
+        title: 'Vv',
+        audio: ' '
+    },
+    {
+        id: 23,
+        title: 'Ww',
+        audio: ' '
+    },
+    {
+        id: 24,
+        title: 'Xx',
+        audio: ' '
+    },
+    {
+        id: 25,
+        title: 'Yy',
+        audio: ' '
+    },
+    {
+        id: 26,
+        title: 'Zz',
+        audio: ' '
+    },
+];
+
+export const AUDIO_FILES: { [key: string]: any } = {
+    //"trees.mp3": require("@/assets/audio/trees.mp3"), etc.
+}
